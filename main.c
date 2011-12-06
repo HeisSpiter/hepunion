bd04e79b871628d1c2ba80d5ef05eb03b7eac4f6
HeisSpiter <HeisSpiter@c7f213ef-2bac-a54b-8fef-5fe711db7e0b>
2011-12-06 17:18:08 +0000

    [PIERREFS] Progress in branches retrieving
    git-svn-id: svn://www3.heisspiter.net/pierrefs-isima@282 c7f213ef-2bac-a54b-8fef-5fe711db7e0b

--- main.c
+++ main.c
@@ -26,11 +26,133 @@ static struct file_system_type pierrefs_fs_type = {
 	.fs_flags	= FS_REVAL_DOT,
 };
 
-static void get_branches(struct super_block *sb, void *raw_data) {
+static int make_path(const char *s, size_t n, char **path) {
+    /* Zero output */
+    *path = 0;
+
+	/* First of all, look if it is relative path */
+	if (s[0] != '/') {
+        return -EINVAL;
+	}
+
+	/* Tailing has to be removed */
+	if (s[n - 1] == '/') {
+		n--;
+	}
+
+	/* Allocate one more ('\0') */
+	*path = kmalloc((n + 1) * sizeof(char));
+	if (*path) {
+		memcpy(*path, s, n);
+		*path[n] = '\0';
+        return 0;
+	}
+
+    return -ENOMEM;
+}
+
+static int get_branches(struct super_block *sb, const char *arg) {
+    int err;
+    char *output;
+    struct pierrefs_sb_info * sb_info = sb->s_fs_info;
+
+    /* We are expecting 2 branches, separated by : */
+	const char *part2 = strchr(arg, ':');
+	if (!part2) {
+		return -EINVAL;
+	}
+
+	/* Look for first branch type */
+	const char *type = strchr(arg, '=');
+	int forced_ro = 0;
+	/* First branch has a type */
+	if (type && type < part2) {
+		/* Get branch name */
+		err = make_path(arg, type - arg, &output);
+		if (err || !ouput) {
+			return err;
+		}
+
+		if (!strncmp(type + 1, "RW", 2)) {
+			sb_info->read_write_branch = output;
+		}
+		else if (strncmp(type + 1, "RO", 2)) {
+			return -EINVAL;
+		}
+		else {
+			sb_info->read_only_branch = output;
+			forced_ro = 1;
+		}
+
+		/* Get type for second branch */
+		type = strchr(part2, '=');
+	}
+	/* It has no type => RO */
+	else {
+		/* Get branch name */
+		err = make_path(arg, part2 - arg, &sb_info->read_only_branch);
+		if (err || !sb_info->read_only_branch) {
+			return err;
+		}
+	}
+
+	/* Skip : */
+	part2++;
+
+	/* If second branch has a type */
+	if (type) {
+		/* Get branch name */
+		err make_path(part2, type - part2, &output);
+		if (err || !output) {
+			return err;
+		}
+
+		if (!strncmp(type + 1, "RW", 2)) {
+			if (sb_info->read_write_branch) {
+				return -EINVAL;
+			}
+			sb_info->read_write_branch = output;
+		}
+		else if (strncmp(type + 1, "RO", 2)) {
+			return -EINVAL;
+		}
+		else {
+			if (forced_ro) {
+				return -EINVAL;
+			}
+			sb_info->read_only_branch = output;
+		}
+	}
+	else {
+		/* It has no type, adapt given the situation */
+		if (sb_info->read_write_branch) {
+			err = make_path(part2, strlen(part2), &sb_info->read_only_branch);
+			if (err || !sb_info->read_only_branch) {
+				return err;
+			}
+		}
+		else if (sb_info->read_only_branch) {
+			err = make_path(part2, strlen(part2), &sb_info->read_write_branch);
+			if (err || !sb_info->read_write_branch) {
+				return err;
+			}
+		}
+	}
+
+	/* At this point, we should have the two branches set */
+	if (!sb_info->read_only_branch || !sb_info->read_write_branch) {
+		return -EINVAL;
+	}
+
+    /* FIXME: test branches validity */
+
+    return 0;
 }
 
 static int pierrefs_read_super(struct super_block *sb, void *raw_data,
 			       int silent) {
+    int err;
+
 	/* Check for parameters */
 	if (!raw_data) {
 		return -EINVAL;
@@ -43,7 +165,18 @@ static int pierrefs_read_super(struct super_block *sb, void *raw_data,
 	}
 
     /* Get branches */
-    get_branches(sb, raw_data);
+    err = get_branches(sb, raw_data);
+    if (err) {
+        if (sb->s_fs_info->read_only_branch) {
+            kfree(sb->s_fs_info->read_only_branch);
+        }
+        if (sb->s_fs_info->read_write_branch) {
+            kfree(sb->s_fs_info->read_write_branch);
+        }
+        kfree(sb->s_fs_info);
+	    sb->s_fs_info = NULL;
+        return err;
+    }
 
 	return -EINVAL;
 }
