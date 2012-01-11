065f842f87ce3e848358220b329459f1632bab91
HeisSpiter <HeisSpiter@c7f213ef-2bac-a54b-8fef-5fe711db7e0b>
2012-01-11 20:29:46 +0000

    [PIERREFS] Start adding helpers for WH & COW
    git-svn-id: svn://www3.heisspiter.net/pierrefs-isima@297 c7f213ef-2bac-a54b-8fef-5fe711db7e0b

---
+++ cow.c
@@ -0,0 +1,41 @@
+/**
+ * \file cow.c
+ * \brief Copy-On-Write (COW) support for the PierreFS file system
+ * \author Pierre Schweitzer <pierre.jean.schweitzer@cern.ch>
+ * \version 1.0
+ * \date 11-Jan-2012
+ * \copyright GNU General Public License - GPL
+ *
+ * Copy-on-write (often written COW) is the mechanism that allows
+ * files of the read-only branch modification. When someone needs
+ * to modify (and can) a file, then a copy of the file (called 
+ * copyup) is created in the read-write branch.
+ *
+ * Next, when the user reads the file, priority is given to the
+ * copyups.
+ *
+ * COW process is also used on directories.
+ *
+ * Unlike all the other implementations of file system unions,
+ * in PierreFS, copyup are not created when an attempt to change
+ * file metadata is done. Metadata are handled separately. This
+ * reduces copyup use.
+ *
+ * Unlike all the other implementations of file system unions,
+ * the PierreFS file system will do its best to try to reduce
+ * redundancy by removing copyup when it appears they are useless
+ * (same contents than the original file).
+ *
+ * This is based on the great work done by the UnionFS driver
+ * team.
+ */
+
+int find_path(const char *path, opt char *real_path) {
+	if (real_path) {
+		return find_path_worker(path, real_path);
+	}
+	else {
+		char tmp_path[PATH_MAX];
+		return find_path_worker(path, tmp_path);
+	}
+}
---
+++ wh.c
@@ -0,0 +1,107 @@
+/**
+ * \file wh.c
+ * \brief Whiteout (WH) support for the PierreFS file system
+ * \author Pierre Schweitzer <pierre.jean.schweitzer@cern.ch>
+ * \version 1.0
+ * \date 11-Jan-2012
+ * \copyright GNU General Public License - GPL
+ *
+ * Whiteout is the mechanism that allows file and directory
+ * deletion on the read-only branch.
+ *
+ * When a demand to delete a file on the read-only branch is
+ * made, the PierreFS file system will a matching whiteout file
+ * on the read-write branch.
+ *
+ * That way, during union, whiteout files will be used to hide
+ * some files from the read-only branch.
+ *
+ * Deleting the whiteout "recovers" the file.
+ *
+ * Whiteouts consist in files called .wh.{original file}
+ *
+ * This is based on the great work done by the UnionFS driver
+ * team.
+ */
+
+static int create_whiteout_worker(const char *wh_path) {
+	int err;
+	struct iattr attr;
+
+	/* Create file */
+	struct file *fd = creat_worker(wh_path, S_IRUSR);
+	if (IS_ERR(fd)) {
+		return fd;
+	}
+
+	/* Set owner to root */
+	attr.ia_valid = ATTR_UID | ATTR_GID;
+	attr.ia_gid = 0;
+	attr.ia_uid = 0;
+
+	err = notify_change(fd->f_dentry->d_inode, &attr);
+	filp_close(fd, 0);
+
+	if (err < 0) {
+		filp_close(fd, 0);
+		vfs_unlink(fd->f_dentry->d_inode, fd->f_dentry);
+		return err;
+	}
+
+	filp_close(fd, 0);
+	return 0;
+}
+
+int create_whiteout(const char *path, char *wh_path) {
+	int err;
+
+	/* Find name */
+	char *tree_path = strrchr(path, '/');
+	if (!tree_path) {
+		return -EINVAL;
+	}
+
+	if (snprintf(wh_path, PATH_MAX, "%s", context.read_write_branch) > PATH_MAX) {
+		return -ENAMETOOLONG;
+	}
+
+	/* Copy path (and /) */
+	strncat(wh_path, path, tree_path - path + 1);
+	/* Append wh */
+	strcat(wh_path, ".wh.");
+	/* Finalement copy name */
+	strcat(wh_path, tree_path + 1);
+
+	/* Ensure path exists */
+	err = find_path(path, NULL);
+	if (err < 0) {
+		return err;
+	}
+
+	/* Call worker */
+	return create_whiteout_worker(wh_path);
+}
+
+int find_whiteout(const char *path, char *wh_path) {
+	struct kstat kstbuf;
+
+	/* Find name */
+	char *tree_path = strrchr(path, '/');
+	if (!tree_path) {
+		return -EINVAL;
+	}
+
+	if (snprintf(wh_path, PATH_MAX, "%s", context.read_write_branch) > PATH_MAX) {
+		return -ENAMETOOLONG;
+	}
+
+	/* Copy path (and /) */
+	strncat(wh_path, path, tree_path - path + 1);
+	/* Append me */
+	strcat(wh_path, ".wh.");
+	/* Finally copy name */
+	strcat(wh_path, tree_path + 1);
+
+	/* Does it exists */
+	return vfs_lstat(wh_path, &kstbuf);
+}
